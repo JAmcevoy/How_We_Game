32b5f6d16bf309a3a173e0065a78990e35519cce
JAmcevoy <>
2024-01-16 12:13:47 +0000

    added the second question to the survey with some error handling

--- run.py
+++ run.py
@@ -19,10 +19,21 @@ def user_questions():
     
     print("Welcome How We Game Survey")
     print("first question is...")
+
+
     console_brand = input("What is your preferred gaming console brand? A)Xbox B)PlayStation C)Nintendo D)PC : ").upper()
     while console_brand not in {'A', 'B', 'C', 'D'}:
         print("Invalid choice. Please choose A, B, C, or D.")
         console_brand = input("What is your preferred gaming console brand? (A/B/C/D): ").upper()
+
+        
+    print("Second question...")
+
+
+    satisfaction_rating = int(input("On a scale of 1 to 10, how satisfied are you with your current gaming console? (1-10): "))
+    while satisfaction_rating <= 1 or satisfaction_rating > 10:
+        print("Invalid choice. Please choose a number between 1 and 10.")
+        satisfaction_rating = int(input("On a scale of 1 to 10, how satisfied are you with your current gaming console? (1-10): "))
 
     
 user_questions()
